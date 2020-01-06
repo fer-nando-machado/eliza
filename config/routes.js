804c003b681e774939f162f84b44b0e54a985bdf
Fernando Machado <lfmachado90@gmail.com>
2020-01-06 01:25:40 -0300

    Stop controller from reloading twice when page changes.

--- config/routes.js
+++ config/routes.js
@@ -1,40 +1,56 @@
 app.config(function ($routeProvider) {
   $routeProvider.
     when(route.login, {
-      templateUrl: 'view/login.html'
+      templateUrl: 'view/login.html',
+      reloadOnUrl: false,
     }).
     when(route.agenda, {
-      templateUrl: 'view/agenda.html'
+      templateUrl: 'view/agenda.html',
+      reloadOnUrl: false,
     }).
     when(route.alunos, {
-      templateUrl: 'view/alunos.html'
+      templateUrl: 'view/alunos.html',
+      reloadOnUrl: false,
     }).
     when(route.professores, {
-      templateUrl: 'view/professores.html'
+      templateUrl: 'view/professores.html',
+      reloadOnUrl: false,
     }).
     when(route.cursos, {
-      templateUrl: 'view/cursos.html'
+      templateUrl: 'view/cursos.html',
+      reloadOnUrl: false,
     }).
     when(route.matriculas, {
-      templateUrl: 'view/matriculas.html'
+      templateUrl: 'view/matriculas.html',
+      reloadOnUrl: false,
     }).
     when(route.pagamentos, {
-      templateUrl: 'view/pagamentos.html'
+      templateUrl: 'view/pagamentos.html',
+      reloadOnUrl: false,
     }).
     when(route.relatorios, {
-      templateUrl: 'view/relatorios.html'
+      templateUrl: 'view/relatorios.html',
+      reloadOnUrl: false,
     }).
     when(route.config, {
-      templateUrl: 'view/config.html'
+      templateUrl: 'view/config.html',
+      reloadOnUrl: false,
     }).
     when(route.credits, {
-      templateUrl: 'view/credits.html'
+      templateUrl: 'view/credits.html',
+      reloadOnUrl: false,
     }).
     when(route.usuarios, {
-      templateUrl: 'view/usuarios.html'
+      templateUrl: 'view/usuarios.html',
+      reloadOnUrl: false,
     }).
     when(route.splash, {
-      templateUrl: 'view/splash.html'
+      templateUrl: 'view/splash.html',
+      reloadOnUrl: false,
+    }).
+    when(route.backup, {
+      templateUrl: 'view/backup.html',
+      reloadOnUrl: false,
     }).
     otherwise({
       redirectTo: route.splash,
