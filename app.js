f935b49ef68f69a78facaf9df49dfc16d804924b
Fernando Machado <lfmachado90@gmail.com>
2020-01-14 17:21:03 -0300

    Updates welcome text.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.run(['$rootScope', 'navigator', 'dao', 'utils', 'users', 'alert',
       if (count == 0) {
         users.useRoot();
         navigator.go(route.usuarios);
-        alert.info("Crie um usuário para começar a usar o Eliza.");
+        alert.info("Crie um usuário para começar a usar o sistema.");
       } else {
         navigator.go(route.login);
       }
