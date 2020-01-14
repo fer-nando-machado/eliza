47360ef8a69161c89c939bbf20d8fd7e04ed72c9
Fernando Machado <lfmachado90@gmail.com>
2020-01-14 17:24:34 -0300

    Do not open DevTools on binary release.

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function createWindow() {
   }));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
   mainWindow.maximize();
 
   // Opens new windows on the native browser
