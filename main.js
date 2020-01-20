9a9853ee197a61f944c26cd7ba428ae3ff6493b4
Fernando Machado <lfmachado90@gmail.com>
2020-01-20 18:17:58 -0300

    Hides menu bar on Windows.

--- main.js
+++ main.js
@@ -28,6 +28,9 @@ function createWindow() {
     },
   });
 
+  // hides the default menu bar
+  mainWindow.setMenu(null);
+
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'app.html'),
